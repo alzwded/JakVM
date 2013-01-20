9fd174666fbd397e475e3f837e916d861fb35066
Vlad <alzwded@gmail.com>
2013-01-20 21:31:59 +0200

    preliminary documenation on file generators

--- grammar-generator/asmGenerator.c
+++ grammar-generator/asmGenerator.c
@@ -1,5 +1,49 @@
 #include <asmGenerator.h>
 
+/*
+    input:  byte stream from parser
+    output: c-source for an executable that takes one parameter
+            (program.a file) and generates binary for VM (program.b)
+
+    structure of output C source file:
+    #include <OUTPUT.h>
+    void check_parameters(argc, argv) {
+        // if(incorrect) abort();
+    }
+    void assemble(argv[0]) {
+        // compute output filename
+        char mnemonic[5], param1[3], param2[20], opcode1[3], opcode2[3], opcode3[3], opcode4[3];
+        short opcodeSize;
+        do {
+            try_read_mnemonic(mnemonic);
+
+            * for_each(typed_t* i in parsedList) generate: *
+            if(strncmp(mnemonic, * generated *) == 0) {
+                * consider reading parameter 1 based on type *
+                * consider reading parameter 2 based on type *
+            } else if( * idem *) {
+            } else {
+                * handle error *
+            }
+
+            if(mnemonic != NULL) {
+                write_instruction(opcodeSize, * opcode1, opcode2, opcode3, opcode4 *);
+            } else {
+                assert(0);
+            }
+        } while(!feof(inFile));
+    }
+    void write_instruction(int, char*, char*, char*, char*) {
+        // write binary
+    }
+    int main(int argc, char* argv[]) {
+        check_parameters(argc, argv);
+        assemble(argv[0]);
+    }
+
+    
+*/
+
 void generateAsmFile()
 {
 }
--- grammar-generator/rtGenerator.c
+++ grammar-generator/rtGenerator.c
@@ -1,5 +1,57 @@
 #include <rtGenerator.h>
 
+/*
+    input:  byte stream from parser
+    output: c-source for an executable that takes one parameter
+            (program.b binary file) and executes it
+            this output program relies on processor.h and everything else
+            to build an executable
+
+    structure of output H file:
+    #include <processor.h>
+    int MNEMONICsIZE_PARAM1_PARAM2(typed_t* i);
+    e.g.:
+    void MOV1_AD_imed(long imed);
+    void MVR1_AD_ED();
+    void LOD4_AX(long b1, long b2, long b3);
+    void CLZ();
+
+    structure of output C file:
+    #include <OUTPUT.h>
+    #include <processor.h>
+    void inc_ip() {
+        // increment IP register
+    }
+    void execute(unisnged char byteStream[]) {
+        long lopcode1, lopcode2, lopcode3, lopcode4;
+        int pos = 0;
+
+        try_read(byteStream, &pos, lopcode2); // calls inc_ip();
+        try_read(byteStream, &pos, lopcode2);
+        switch(lopcode1) {
+        case * generated *:
+            * either require reading 3 additional immediate values *
+            * or this is enough to execute *
+            * or switch remaining opcode2 *
+        case 0x01:
+            MOV_1_AD_imed(lopcode2);
+            break;
+        case 0x1D:
+            switch(lopcode2) {
+            case 0x09:
+                MVR_1_AB_EC();
+                break;
+            }
+            break;
+        }
+        case 0xE0:
+            try_read(byteStream, &pos, lopcode3);
+            try_read(byteStream, &pos, lopcode4);
+            LOD1_AD_add(lopcode2, lopcode3, lopcode4);
+            break;
+    }
+*/
+
 void generateRTFile()
 {
 }
